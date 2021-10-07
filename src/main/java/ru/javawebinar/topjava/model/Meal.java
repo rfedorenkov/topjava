bf77812cd81fcb9818ef35abe6ce59fc422b043a
Rostislav Fedorenkov <fedorenkovrostislav@gmail.com>
2021-10-07 12:22:23 +0300

    Apply patch MealsUtil_opt 1_opt_3_HW0_opt2

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -9,7 +9,13 @@ public class Meal {
 
     private final String description; // описание
 
-    private final int calories; // калории
+    private final int calories;
+
+    //    private final boolean excess;
+//    private final AtomicBoolean excess;      // filteredByAtomic (or any ref type, e.g. boolean[1])
+//    private final Boolean excess;            // filteredByReflection
+    //    private final Supplier<Boolean> excess;  // filteredByClosure
+    private boolean excess;
 
     public Meal(LocalDateTime dateTime, String description, int calories) {
         this.dateTime = dateTime;
